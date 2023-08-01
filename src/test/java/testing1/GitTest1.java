98be6f4f7d6c17ae4b11cc4b06bdc7f9fdc76cfa
MuruganMara <muruganmara008@gmail.com>
2023-08-01 23:19:54 +0530

    Testin git

---
+++ src/test/java/testing1/GitTest1.java
@@ -0,0 +1,15 @@
+package testing1;
+
+public class GitTest1 {
+	
+
+	public static void main(String[] args) {
+		System.out.println("1234");
+		System.out.println("ghjkl");
+		System.out.println("hjkl456");
+		System.out.println("678hmbnm234");
+		
+	}
+
+
+}
